2e791c1d7c20912eb00691c359ac3e5f22cded00
Mickey Thein <mickeyminthein@gmail.com>
2026-02-17 14:12:24 -0500

    changed to ssh link

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,12 +6,12 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 89fd3009fd30364cfcda149939ac0bd6ec689a67
+AESD_ASSIGNMENTS_VERSION = 16f12d1f1eb81b6c4cc33f9aedc1705661d1bf7e
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-mickeyminthein.git
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-mickeyminthein.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
